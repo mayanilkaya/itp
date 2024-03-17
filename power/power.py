e0a82d8e29007d935fd1465b0de57b62819d78a0
mayanilkaya <mayanilkaya@gmail.com>
2024-03-17 18:08:03 -0400

    documentation and file

---
+++ power/power.py
@@ -0,0 +1,16 @@
+for i in range (-8, 9):
+      print(i, end=" ")
+print()
+for b in range(-8, 0):
+      for a in range(-8, 9):
+            if a>= b:
+                  print(b, end=' ')
+                  print () 
+def get_power(x, n):
+        return x ** n
+print(get_power (x, 2))
+
+def print_graph(n):
+    return('*' * n) 
+print(print_graph)
+
